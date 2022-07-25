790d1bdf063f359ed78be2fc2ba31d84da4e126f
killshotxd <hassanansari211@gmail.com>
2022-07-25 10:37:55 +0530

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,28 @@
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.jpg";
+import HeaderSocials from "./HeaderSocials";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mohd Hassan</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,19 @@
+import { BsLinkedin, BsGithub, BsFacebook } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__social">
+      <a href="#linkedin" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="#github" target="_blank">
+        <BsGithub />
+      </a>
+      <a href="#fb" target="_blank">
+        <BsFacebook />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
